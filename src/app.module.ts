af3cfcf192634d36be96da18e7b6b000ee68aa9d
Viktor Grant <isahara86@gmail.com>
2022-11-22 22:37:58 +0100

    feat: add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileModule } from './file/file.module';
+import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
   imports: [FileModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { APP_NAME, APP_VERSION } from './environment';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return `${APP_NAME} v${APP_VERSION}`;
+  constructor(private readonly db: PrismaService) {}
+
+  async getHello(): Promise<string> {
+    const res = await this.db.$queryRaw`SELECT now()`;
+
+    return `${APP_NAME} v${APP_VERSION} ${JSON.stringify(res)}`;
   }
 }
