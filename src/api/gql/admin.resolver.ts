c8faa47e4db4b8a5db9d8eec710ae1e074726883
Viktor Grant <isahara86@gmail.com>
2022-12-09 14:49:36 +0100

    feat: invite admin

--- src/api/gql/admin.resolver.ts
+++ src/api/gql/admin.resolver.ts
@@ -3,6 +3,9 @@ import { Admin } from './models/admin.model';
 import { AdminLoginResponse } from './models/admin-login-response.model';
 import { AuthService } from '../../modules/auth/auth.service';
 import { AdminLoginInput } from './inputs/admin-login.input';
+import { AdminInviteInput } from './inputs/admin-invite.input';
+import { SuccessModel } from './models/success.model';
+import { BadRequestException } from '@nestjs/common';
 
 @Resolver(of => Admin)
 export class AdminResolver {
@@ -11,5 +14,11 @@ export class AdminResolver {
   @Mutation(returns => AdminLoginResponse)
   adminLogin(@Args('input') input: AdminLoginInput): Promise<AdminLoginResponse> {
     return this.authService.adminLogin(input);
+  }
+
+  @Mutation(returns => SuccessModel)
+  async inviteAdmin(@Args('input') input: AdminInviteInput): Promise<SuccessModel> {
+    await this.authService.registerAdmin(input);
+    return { success: true };
   }
 }
